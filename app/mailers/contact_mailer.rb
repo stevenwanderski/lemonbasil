42721da17828c54d23e2ea48a8e3e9c6f124ed02
Steven <steven.wanderski@gmail.com>
2020-09-09 22:15:25 -0500

    Updates contact mailer email

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -4,7 +4,7 @@ class ContactMailer < ApplicationMailer
 
     mail(
       from: 'Lemon&Basil <hello@lemonandbasil.co>',
-      to: 'Birdie <wandoledzep@gmail.com>',
+      to: 'Birdie <steven.wanderski@gmail.com>',
       subject: 'New contact message from lemonandbasil.co'
     )
   end

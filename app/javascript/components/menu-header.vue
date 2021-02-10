bad0fbff258bf1687cebdb39da73433a9c8b6063
Steven <steven.wanderski@gmail.com>
2021-02-10 15:46:31 -0600

    Adds menu form to menu-header

--- app/javascript/components/menu-header.vue
+++ app/javascript/components/menu-header.vue
@@ -1,37 +1,86 @@
 <template>
-  <div class="menu-header">
-    <div class="menu-header__name">
-      {{menu.client_full_name}}
-    </div>
+  <div>
+    <div class="menu-header">
+      <div class="menu-header__name">
+        {{menu.client_full_name}}
+      </div>
 
-    <div class="menu-header__date">
-      {{menu.due_at | moment('MMMM D, Y')}}
-    </div>
+      <div class="menu-header__date">
+        {{menu.due_at | moment('MMMM D, Y')}}
+      </div>
 
-    <div class="menu-header__nav hamburger-nav">
-      <button class="hamburger-nav__control" @click="toggleNav">
-        <span></span>
-        <span></span>
-        <span></span>
-      </button>
+      <div class="menu-header__nav hamburger-nav">
+        <button class="hamburger-nav__control" @click="toggleNav">
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
 
-      <div class="hamburger-nav__body" v-if="navOpen">
-        <a href="">Edit</a>
-        <a href="">Copy</a>
+        <div class="hamburger-nav__body" v-if="navOpen">
+          <a href="" @click="clickEdit($event)">Edit</a>
+          <a href="">Copy</a>
+        </div>
       </div>
     </div>
+
+    <modal v-if="showingModal" :onClose="closeModal">
+      <h2>Edit Client Menu</h2>
+      <form @submit="checkForm">
+        <div class="form-item">
+          <label for="due_at" class="label">Prep Date</label>
+          <datepicker
+            name="due_at"
+            format="MM/dd/yyyy"
+            input-class="input--text"
+            :typeable="true"
+            v-model="dueAt"
+          ></datepicker>
+        </div>
+
+        <div class="form-actions">
+          <button class="button button--submit">
+            Submit
+          </button>
+
+          <button @click="closeModal" class="link">Cancel</button>
+        </div>
+      </form>
+    </modal>
   </div>
 </template>
 
 <script>
+  import Datepicker from 'vuejs-datepicker';
+  import Modal from './modal';
+
   export default {
+    components: {
+      Datepicker,
+      Modal
+    },
+
     data() {
       return {
-        navOpen: false
+        dueAt: null,
+        navOpen: false,
+        showingModal: false
       }
     },
 
     methods: {
+      checkForm() {
+
+      },
+
+      clickEdit(event) {
+        event.preventDefault();
+        this.showingModal = true;
+      },
+
+      closeModal() {
+        this.showingModal = false;
+      },
+
       toggleNav() {
         this.navOpen = !this.navOpen;
       }

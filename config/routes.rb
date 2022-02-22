f063e597c3c34f01e1b47e196256c8f3707b5f8a
Steven Wanderski <steven.wanderski@gmail.com>
2022-02-21 20:25:46 -0600

    Removes menus and pricing pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,9 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   get '/about', to: 'pages#about'
-  get '/menus', to: 'pages#menus'
   get '/meal-prep', to: 'pages#meal_prep'
   get '/parties', to: 'pages#parties'
   get '/calendar', to: 'pages#calendar'
-  get '/pricing', to: 'pages#pricing'
 
   get '/contact', to: 'contact#new'
   post '/contact', to: 'contact#create'

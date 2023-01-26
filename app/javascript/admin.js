89fdb9854ade8c02f41c87068679990d5073c519
Steven Wanderski <steven.wanderski@gmail.com>
2023-01-26 11:24:05 -0600

    Fixes up admin section

--- app/javascript/admin.js
+++ app/javascript/admin.js
@@ -4,54 +4,6 @@ import "@hotwired/turbo-rails"
 import Rails from '@rails/ujs';
 Rails.start();
 
-class AdminCalendar {
-  constructor(args) {
-    this.token = args.token;
-    this.initializeCalendar();
-  }
-
-  initializeCalendar(events) {
-    const element = document.getElementById('calendar');
-
-    this.calendar = new FullCalendar.Calendar(element, {
-      initialView: 'dayGridMonth',
-      eventSources: [
-        {
-          url: '/api/events'
-        }
-      ],
-
-      loading: (isLoading) => {
-        if (isLoading) {
-          $('.loading').show();
-        } else {
-          $('.loading').hide();
-        }
-      },
-
-      dateClick: (info) => {
-        const data = {
-          date: info.dateStr
-        }
-
-        fetch('/api/events', {
-          method: 'POST',
-          headers: {
-            'Authorization': `Bearer ${this.token}`,
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(data),
-        })
-        .then((response) => {
-          this.calendar.refetchEvents();
-        })
-      }
-    });
-
-    this.calendar.render();
-  }
-}
-
 $(function() {
   $('.datepicker').datepicker({
     dateFormat: 'm/d/yy'
